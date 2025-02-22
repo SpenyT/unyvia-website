75157cab465ffa55d77698ed1d9baf7747fef485
Ambrose821 <mclaughlinambrose@gmail.com>
2025-02-22 11:14:20 -0500

    Added animations to desktop and mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import LogoName from './LogoName';
 
 import '../styles/componentStyles/navbar.css';
-import { useState } from 'react';
+import {  useState } from 'react';
 
 
 // const ProductLinks = () => {
@@ -17,24 +17,30 @@ import { useState } from 'react';
 
 
 
+
 export default function Navbar() {
-    const [menuToggle, setMenuToggle] = useState<boolean>(false);
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+    const [hamburgerClicked, setHamburgerClicked] = useState<boolean>(false);
         
+    const toggleMenu = () =>{
+        setMenuOpen(!menuOpen)
+        setHamburgerClicked(true)
+        // console.log(menuOpen)
+    }
     return (
         <header className="navbar">
             <nav className="navbar-container">
                 <Link to="/">
                     <div className="logo"><LogoName /></div>
                 </Link>
-                <div className={menuToggle ?  "navbar-buttons" :"navbar-buttons-open"}>
+                <div className={menuOpen ? "navbar-buttons-open" : hamburgerClicked ? "navbar-buttons-closed" :  "navbar-buttons" }>
+
                     <Link to="/"><button className="navbar-button">Home</button></Link>
                     <Link to="/lighting"><button className="navbar-button">Lighting</button></Link>
                     <Link to="/hvac"><button className="navbar-button">Hvac</button></Link>
                     <Link to="/about-us"><button className="navbar-button">About Us</button></Link>
                 </div>
-                <div className="menu-btn" onClick={()=>{
-                    setMenuToggle(!menuToggle)
-                }}> 
+                <div className={`menu-btn ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}> 
                      <span className="bar top"></span>
                      <span className="bar middle"></span>
                      <span className="bar bottom"></span>
