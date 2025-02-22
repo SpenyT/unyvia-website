cc26d57abd16bed46140120d7932fb5fe65be04b
Ambrose821 <mclaughlinambrose@gmail.com>
2025-02-22 01:03:55 -0500

    Added Responsive react navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
+
 import LogoName from './LogoName';
 
 import '../styles/componentStyles/navbar.css';
+import { useState } from 'react';
 
 
 // const ProductLinks = () => {
@@ -16,18 +18,26 @@ import '../styles/componentStyles/navbar.css';
 
 
 export default function Navbar() {
-
+    const [menuToggle, setMenuToggle] = useState<boolean>(false);
+        
     return (
         <header className="navbar">
             <nav className="navbar-container">
                 <Link to="/">
                     <div className="logo"><LogoName /></div>
                 </Link>
-                <div className="navbar-buttons">
+                <div className={menuToggle ?  "navbar-buttons" :"navbar-buttons-open"}>
                     <Link to="/"><button className="navbar-button">Home</button></Link>
                     <Link to="/lighting"><button className="navbar-button">Lighting</button></Link>
                     <Link to="/hvac"><button className="navbar-button">Hvac</button></Link>
                     <Link to="/about-us"><button className="navbar-button">About Us</button></Link>
+                </div>
+                <div className="menu-btn" onClick={()=>{
+                    setMenuToggle(!menuToggle)
+                }}> 
+                     <span className="bar top"></span>
+                     <span className="bar middle"></span>
+                     <span className="bar bottom"></span>
                 </div>
             </nav>
         </header>
