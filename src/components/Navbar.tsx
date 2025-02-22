a07a64516bf14e04aa263aab7d984e45eb8f8241
Ambrose821 <mclaughlinambrose@gmail.com>
2025-02-22 00:10:13 -0500

    Added responsive hamburger button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,16 +19,32 @@ export default function Navbar() {
 
     return (
         <header className="navbar">
+        <input type="checkbox" className="check" id="checked"/>
+        <label className="menu-btn" htmlFor="checked">
+            <span className="bar top"></span>
+            <span className="bar middle"></span>
+            <span className="bar bottom"></span>
+            <span className="menu-btn__text"></span>
+        </label>
+        <label className="close-menu" htmlFor="checked"></label>
             <nav className="navbar-container">
-                <Link to="/">
-                    <LogoName />
-                </Link>
+                <div className='logo'>
+                    <Link to="/">
+                        <LogoName />
+                    </Link>
+                </div>
+               
                 <div className="navbar-buttons">
                     <Link to="/"><button className="navbar-button">Home</button></Link>
                     <Link to="/lighting"><button className="navbar-button">Lighting</button></Link>
                     <Link to="/hvac"><button className="navbar-button">Hvac</button></Link>
                     <Link to="/about-us"><button className="navbar-button">About Us</button></Link>
                 </div>
+
+                
+
+                
+              
             </nav>
         </header>
     );
