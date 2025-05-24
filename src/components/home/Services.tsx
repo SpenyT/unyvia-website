9e28db5555b6f06c3cce1b490e03f9daddd4b046
Ambrose821 <mclaughlinambrose@gmail.com>
2025-05-23 18:40:44 -0700

    Added Basic service Cards

--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import '../../styles/pageStyles/home/home.css';
 import '../../styles/pageStyles/home/services.css'
-
+import LightingLogo from '../../assets/lighting-icon.svg?react'
+import EnergyAuditLogo from '../../assets/energy-audit.svg?react'
+import HVACLogo from '../../assets/hvac.svg?react'
+import LEDLogo from '../../assets/led.svg?react'
+import DecarbonLogo from '../../assets/decarbon.svg?react'
+import EnergyAsaService from '../../assets/eas.svg?react'
+import SolarLogo from '../../assets/solar.svg?react'
 // import Hero from '../components/home/Hero.tsx';
 
 
@@ -19,25 +25,142 @@ export default function Services() {
                 <div className="divider"></div>
                 <div className="service-menu">
                     <div className="service-menu-card">
-                        <div className="service-menu-card-title">Energy Audits & SubsidiesXS</div>
+                        <div className="service-menu-card-title"><h4>Energy Audits & Subsidies</h4></div>
                         <div className="service-menu-card-subtitle">Optimize. Save. Sustain. </div>
-                        <div className="service-menu-card-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptas voluptatibus hic possimus, temporibus officia soluta consequuntur architecto doloremque? Optio modi distinctio incidunt? Eligendi possimus, nam pariatur enim magni nulla.</div>
+                        <div className="service-menu-card-body">
+                            <div className="service-icon">
+                                <EnergyAuditLogo className='logo-icon'/>
+                            </div>
+                            <div className='service-info'> 
+                                <ul>
+                                    <li>Level I, II, III energy audits</li>
+                                    <li>Customized reports with actionable plans</li>
+                                    <li>Subsidy and grant application support</li>
+                                    <li>Energy performance tracking</li>
+                                    <li>ROI-focused upgrade recommendations</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="service-menu-card-cta">
+                            Learn More 
+                        </div>
                    </div>
                          
                     <div className="service-menu-card">
-                        <h4>HVAC System Optimization </h4>
+                        <div className="service-menu-card-title"><h4>HVAC System Optimization </h4></div>
+                        <div className="service-menu-card-subtitle">Breathe easier. Spend smarter. </div>
+                        <div className="service-menu-card-body">
+                            <div className="service-icon">
+                                <HVACLogo className='logo-icon'/>
+                            </div>
+                            <div className='service-info'> 
+                            <ul>
+                                <li>HVAC design and retrofitting</li>
+                                <li>Smart controls & predictive maintenance</li>
+                                <li>Integrated Design-Build execution</li>
+                                <li>Indoor air quality enhancements</li>
+                                <li>Ongoing maintenance and monitoring</li>
+                            </ul>
+
+                            </div>
+                        </div>
+                        <div className="service-menu-card-cta">
+                            Learn More 
+                        </div>
                     </div>
                     <div className="service-menu-card">
-                        <h4>LED Lighting Solutions </h4>
+                         <div className="service-menu-card-title"><h4>LED Lighting Solutions</h4></div>
+                        <div className="service-menu-card-subtitle">Light smarter. Shine brighter.</div>
+                        <div className="service-menu-card-body">
+                            <div className="service-icon">
+                                <LEDLogo className='logo-icon'/>
+                            </div>
+                            <div className='service-info'> 
+                            <ul>
+                                <li>LED retrofits and new lighting design</li>
+                                <li>Photometric and application engineering</li>
+                                <li>Emergency and high-performance lighting</li>
+                                <li>Direct distributor pricing</li>
+                                <li>Inventory and logistics optimization (VMI)</li>
+                            </ul>
+
+
+                            </div>
+                        </div>
+                        <div className="service-menu-card-cta">
+                            Learn More 
+                        </div>
                     </div>
                     <div className="service-menu-card">
-                        <h4>Energy-as-a-Service (EaaS) Financing  </h4>
+                        <div className="service-menu-card-title"><h4>Decarbonization Services </h4></div>
+                        <div className="service-menu-card-subtitle">Your path to net zero starts here. </div>
+                        <div className="service-menu-card-body">
+                            <div className="service-icon">
+                                <DecarbonLogo className='logo-icon'/>
+                            </div>
+                            <div className='service-info'> 
+                         <ul>
+                            <li>Carbon footprint analysis</li>
+                            <li>Strategic reduction planning</li>
+                            <li>Full implementation support</li>
+                            <li>Regulatory and certification compliance (LEED, Zero Carbon)</li>
+                            <li>Monitoring and performance optimization</li>
+                        </ul>
+
+
+                            </div>
+                        </div>
+                        <div className="service-menu-card-cta">
+                            Learn More 
+                        </div>
                     </div>
                     <div className="service-menu-card">
-                        <h4>ecarbonization Services  </h4>
+                         <div className="service-menu-card-title"><h4>Energy-as-a-Service (EaaS) Financing </h4></div>
+                        <div className="service-menu-card-subtitle">No CAPEX. No debt. No delays. </div>
+                        <div className="service-menu-card-body">
+                            <div className="service-icon">
+                                <EnergyAsaService className='logo-icon'/>
+                            </div>
+                            <div className='service-info'> 
+                                <ul>
+                                    <li>Zero-CAPEX project financing</li>
+                                    <li>Fixed monthly payments</li>
+                                    <li>Immediate operational savings</li>
+                                    <li>Full project implementation and maintenance</li>
+                                    <li>$1 buyback at end of term</li>
+                                </ul>
+
+
+
+                            </div>
+                        </div>
+                        <div className="service-menu-card-cta">
+                            Learn More 
+                        </div>
                     </div>
                     <div className="service-menu-card">
-                        <h4>Solar Panels & Energy Storage Solutions  </h4>
+                         <div className="service-menu-card-title"><h4>Solar Panels & Energy Storage Solutions</h4></div>
+                        <div className="service-menu-card-subtitle">Power your future—naturally.</div>
+                        <div className="service-menu-card-body">
+                            <div className="service-icon">
+                                <SolarLogo className='logo-icon'/>
+                            </div>
+                            <div className='service-info'> 
+                               <ul>
+                                    <li>Solar PV design and installation</li>
+                                    <li>Battery storage and grid independence</li>
+                                    <li>Smart energy management systems</li>
+                                    <li>Performance monitoring and support</li>
+                                    <li>EaaS financing options</li>
+                                </ul>
+
+
+
+                            </div>
+                        </div>
+                        <div className="service-menu-card-cta">
+                            Learn More 
+                        </div>
                     </div>
                     
 
