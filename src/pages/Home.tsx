e875dd2f7bd298f534114558d800f269c2fdd7c7
Holocronic <106043811+Holocronic@users.noreply.github.com>
2025-06-17 14:51:03 -0400

    Streamlined Homepage structure + Hero placeholder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import '../styles/pageStyles/home/home.css';
 import Services from '../components/home/Services';
+import Hero from '../components/home/Hero.tsx';
 
-// import Hero from '../components/home/Hero.tsx';
-
+import '../styles/pageStyles/home/home.css';
 
 
 export default function Home() {
 
     return (
         <div className="home">
-            <div className="content">
-             <Services></Services>
-            </div>
+            <Hero/>
+            <Services/>
         </div>
     );
 }
